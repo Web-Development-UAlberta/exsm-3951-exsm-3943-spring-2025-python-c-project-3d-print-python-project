26313a1d680f0e758d775e671ac7444392f1ec81
Noah <noahc1@hotmail.com>
2025-05-02 19:02:43 -0600

    Implement CRUD functions for materials table

---
+++ src/print_shop/store/CRUD/materials_view.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect, get_object_or_404, messages
+from .forms import MaterialsForm
+from ..models import Materials
+
+# List all materials
+def materials_list(request):
+    materials = Materials.objects.all()
+    return render(request, 'materials/materials_list.html', {'materials': materials})
+
+# Create a new material
+def add_material(request):
+    if request.method == 'POST':
+        form = MaterialsForm(request.POST)
+        if form.is_valid():
+            material = form.save()
+            messages.success(request, f'Material {material.Name} was created successfully')
+            return redirect('materials-list')
+    else:
+        form = MaterialsForm()
+    return render(request, 'materials/material_form.html', {'form': form})
+
+# Edit an existing material
+def edit_material(request, pk):
+    material = get_object_or_404(Materials, pk=pk)
+    if request.method == 'POST':
+        form = MaterialsForm(request.POST, instance=material)
+        if form.is_valid():
+            material = form.save()
+            messages.success(request, f'Material {material.Name} was updated successfully')
+            return redirect('materials-list')
+    else:
+        form = MaterialsForm(instance=material)
+    return render(request, 'materials/material_form.html', {'form': form})
+
+
+# Delete a material
+def delete_material(request, pk):
+    material = get_object_or_404(Materials, pk=pk)
+    if request.method == 'POST':
+        material.delete()
+        messages.success(request, f'Material {material.Name} was deleted successfully')
+        return redirect('materials-list')
+    return render(request, 'materials/material_confirm_delete.html', {'material': material})
+
+
