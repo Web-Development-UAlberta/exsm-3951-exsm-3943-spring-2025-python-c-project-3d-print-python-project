f24ba02150d40be532098d7d643715105af7bc87
Noah <noahc1@hotmail.com>
2025-05-07 12:18:55 -0600

    Update URLs for materials and file path in material view

--- src/print_shop/store/urls.py
+++ src/print_shop/store/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views.CRUD import models_view
 from .views.CRUD import suppliers_view
+from .views.CRUD import materials_view
 
 urlpatterns = [
     path("", models_view.models_list, name="models-list"),
@@ -13,4 +14,9 @@ urlpatterns = [
     path("suppliers/add/", suppliers_view.add_supplier, name="add-supplier"),
     path("suppliers/edit/<int:pk>/", suppliers_view.edit_supplier, name="edit-supplier"),
     path("suppliers/delete/<int:pk>/", suppliers_view.delete_supplier, name="delete-supplier"),
+
+    path("materials/", materials_view.materials_list, name="materials-list"),
+    path("materials/add/", materials_view.add_material, name="add-material"),
+    path("materials/edit/<int:pk>/", materials_view.edit_material, name="edit-material"),
+    path("materials/delete/<int:pk>/", materials_view.delete_material, name="delete-material"),
 ]
