8084eb0618ab0f736c9a2ebc0a1cc816cbd5e5eb
Jarett <jarett.rude@gmail.com>
2025-05-07 08:13:42 -0700

    Chore: clean up imports in models.py

--- src/print_shop/store/models.py
+++ src/print_shop/store/models.py
@@ -7,7 +7,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.validators import RegexValidator, MinValueValidator
-from django.db.models import Max, OuterRef, Subquery
 
 
 class Materials(models.Model):
