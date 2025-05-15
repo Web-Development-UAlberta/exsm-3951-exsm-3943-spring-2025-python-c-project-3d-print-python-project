e311410d543ae30d533f27836e9885bb4dc65c45
Athira C A <athiraca1@gmail.com>
2025-05-14 22:44:24 -0600

    Add authentication for admin dashboard page

--- src/print_shop/store/views/admin_dashboard_view.py
+++ src/print_shop/store/views/admin_dashboard_view.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required, user_passes_test
 from store.models import Orders, FulfillmentStatus, Models, InventoryChange 
 
+# Check if the user is admin (Staff or Superuser)
+def is_admin(user):
+    return user.is_authenticated and (user.is_superuser or user.is_staff)
 
+@login_required
+@user_passes_test(is_admin)
 def admin_dashboard(request):
     total_orders = Orders.objects.count()
 
