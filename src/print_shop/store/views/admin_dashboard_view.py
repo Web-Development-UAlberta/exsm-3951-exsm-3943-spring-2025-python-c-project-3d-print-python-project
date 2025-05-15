17161e445c54413ce780864de5b02a86ede5b58a
Athira C A <athiraca1@gmail.com>
2025-05-14 22:55:37 -0600

    Uncomment authentication for Admin Dashboard

--- src/print_shop/store/views/admin_dashboard_view.py
+++ src/print_shop/store/views/admin_dashboard_view.py
@@ -6,8 +6,8 @@ from store.models import Orders, FulfillmentStatus, Models, InventoryChange
 def is_admin(user):
     return user.is_authenticated and (user.is_superuser or user.is_staff)
 
-# @login_required
-# @user_passes_test(is_admin)
+@login_required
+@user_passes_test(is_admin)
 def admin_dashboard(request):
     total_orders = Orders.objects.count()
 
