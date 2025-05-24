3dae6c1651f1bc9c5c9aa40351f389801049eaf5
Jarett <jarett.rude@gmail.com>
2025-05-24 11:46:26 -0700

    Fix: test user delete

--- src/print_shop/store/tests/test_frontend_admin_User_Management.py
+++ src/print_shop/store/tests/test_frontend_admin_User_Management.py
@@ -63,8 +63,8 @@ class UserManagementFrontendTestCase(StaticLiveServerTestCase):
     def test_user_delete_view_redirects(self):
         self.browser.get(self.live_server_url + reverse("user-profile-list"))
         header = WebDriverWait(self.browser, 20).until(
-            EC.visibility_of_element_located((By.TAG_NAME, "a")))
-        delete_button = self.browser.find_element(By.LINK_TEXT, "Delete")
+            EC.visibility_of_element_located((By.CLASS_NAME, "user-delete-button")))
+        delete_button = self.browser.find_element(By.CLASS_NAME, "user-delete-button")
         delete_button.click()
         time.sleep(1)
         self.assertIn("delete the user profile", self.browser.page_source)
