888feeb52139f2a953f045f40e7aa8ae5d477a26
Jarett <jarett.rude@gmail.com>
2025-05-25 18:26:13 -0700

    Make thumbnail optional to match models.py and tests

--- src/print_shop/store/forms/models_form.py
+++ src/print_shop/store/forms/models_form.py
@@ -3,6 +3,8 @@ from store.models import Models
 
 
 class ModelsForm(forms.ModelForm):
+    Thumbnail = forms.ImageField(required=False)
+    
     class Meta:
         model = Models
         fields = "__all__"
