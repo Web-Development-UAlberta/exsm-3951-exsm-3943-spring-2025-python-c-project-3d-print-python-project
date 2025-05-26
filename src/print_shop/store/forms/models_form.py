3ed719e89a056e3384d119eedd9bba38978c1499
Jarett <jarett.rude@gmail.com>
2025-05-25 17:42:10 -0700

    Refactor: make model form show all fields

--- src/print_shop/store/forms/models_form.py
+++ src/print_shop/store/forms/models_form.py
@@ -5,11 +5,4 @@ from store.models import Models
 class ModelsForm(forms.ModelForm):
     class Meta:
         model = Models
-        fields = [
-            "Name",
-            "Description",
-            "FilePath",
-            "FixedCost",
-            "EstimatedPrintVolume",
-            "BaseInfill",
-        ]
+        fields = "__all__"
